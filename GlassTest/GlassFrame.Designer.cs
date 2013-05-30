8b56029092bf76574fa21582c95769d53b2eade7
zwerch <zwerch1337@googlemail.com>
2013-05-31 00:20:18 +0200

    init commit

---
+++ GlassTest/GlassFrame.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GlassTest
+{
+    partial class GlassFrame
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(403, 162);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This text is just to\r\nshow some text on\r\nthe glassed control.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GlassFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(403, 162);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "GlassFrame";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Important Information";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmMain_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GlassTest/GlassFrame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GlassTest
+{
+    public partial class GlassFrame : Form
+    {
+        public GlassFrame()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The API used to extend the Glass margins into the client area
+        /// </summary>
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        public static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
+
+        /// <summary>
+        /// Determines whether the Desktop Windows Manager is enabled
+        /// and can therefore display Aero 
+        /// </summary>
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        public static extern bool DwmIsCompositionEnabled();
+
+        /// <summary>
+        /// The struct used to pass the Glass margins to the Win32 API
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MARGINS
+        {
+            public int Left;
+            public int Right;
+            public int Top;
+            public int Bottom;
+        }
+
+        /// <summary>
+        /// Use the form padding values to define a Glass margin
+        /// </summary>
+        private void SetGlass()
+        {
+            if (DwmIsCompositionEnabled())
+            {
+                MARGINS margins = new MARGINS();
+                margins.Top = -1; margins.Left = -1; margins.Bottom = -1; margins.Right = -1;
+                DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+            }
+        }
+
+        // When Form is painted, SetGlass()
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            SetGlass();
+        }
+    }
+}
